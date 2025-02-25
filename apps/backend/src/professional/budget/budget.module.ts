a81fad3b4cf38f8942538db3d30e06a1c9147cd9
Rodrigo <rodrigo22matos59@gmail.com>
2025-02-25 00:25:05 -0300

    Aiai

--- apps/backend/src/professional/budget/budget.module.ts
+++ apps/backend/src/professional/budget/budget.module.ts
@@ -3,12 +3,10 @@ import { BudgetService } from './budget.service';
 import { BudgetResolver } from './budget.resolver';
 import { PrismaService } from '@/prisma/prisma.service'; 
 import { UnitModule } from './unit/unit.module';
-import { ProfessionalModule } from '@professional/professional.module';
-
 
 @Module({
   providers: [BudgetResolver, BudgetService, PrismaService],
   exports: [BudgetService],
-  imports: [UnitModule, ProfessionalModule],
+  imports: [UnitModule],
 })
 export class BudgetModule {}
